bd2037b531acd0ba3a197ecbe6e71d8c2d488a60
Youness Amzil <amzilyouness@gmail.com>
2025-06-11 22:38:07 +0100

    Improved the dashboard.

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import {
 	Search, Plus, Filter, Edit, Trash2, ChevronDown,
-	ChevronUp, Download, FileSpreadsheet, ChevronLeft, ChevronRight
+	ChevronUp, FileSpreadsheet, ChevronLeft, ChevronRight
 } from 'lucide-react';
 import { useLanguage } from '../../context/LanguageContext';
 import AddClientModal from '../../components/AddClientModal';
@@ -172,7 +172,7 @@ export default function ClientsPage() {
 							</div>
 							<input
 								type="text"
-								className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+								className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm text-black"
 								placeholder={t('clients.search')}
 								value={searchQuery}
 								onChange={(e) => setSearchQuery(e.target.value)}
