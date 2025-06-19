4631e28257f44ce382e167033d9b76b46ac0eedc
Youness Amzil <amzilyouness@gmail.com>
2025-06-19 21:14:46 +0100

    project version on status bar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
 			<div className="container mx-auto flex justify-between items-center">
 				<div className="flex items-center text-xs text-gray-500">
 					<div className="h-2 w-2 rounded-full bg-green-500 mr-2"></div>
-					<span>DataFlow v1.0.0</span>
+					<span>DataFlow v0.1.0</span>
 				</div>
 				<div className="text-xs text-gray-500">
 					© 2025 DataFlow
