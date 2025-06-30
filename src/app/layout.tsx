f08960e7660b018a24f751368296f74f56406900
Youness Amzil <amzilyouness@gmail.com>
2025-06-30 06:21:22 +0100

    First checkpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,22 @@ import { LanguageProvider } from '../context/LanguageContext'
 import '../styles/globals.css'
 import LayoutWrapper from '../components/LayoutWrapper'
 
+export const metadata = {
+	title: 'DataFlow - Client Management Solution',
+	description: 'Client management and money calculation system',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <LanguageProvider>
-          <Toaster position="top-right" containerStyle={{ top: 80 }} />
-          <LayoutWrapper>
-            {children}
-          </LayoutWrapper>
-        </LanguageProvider>
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body>
+				<LanguageProvider>
+					<Toaster position="top-right" containerStyle={{ top: 80 }} />
+					<LayoutWrapper>
+						{children}
+					</LayoutWrapper>
+				</LanguageProvider>
+			</body>
+		</html>
+	)
 }
